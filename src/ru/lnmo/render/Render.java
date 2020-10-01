be5306cf8034bdfb4eab9d9dc2fc2e30dc979f74
xitriyEvrey <aqlwiansalen@gmail.com>
2020-10-01 20:18:21 +0300

    Добавлены классы Vector и Matrix, переделан метод renderTriangle

--- src/ru/lnmo/render/Render.java
+++ src/ru/lnmo/render/Render.java
@@ -51,14 +51,14 @@ public class Render {
 
 
     public static void renderTriangle(BufferedImage img, int x1, int y1, int x2, int y2, int x3, int y3, Color color){
-       Vector2D AB = new Vector2D(x2, y2).sum(new Vector2D(x1, y1).multSc(-1));
-       Vector2D AC = new Vector2D(x3, y3).sum(new Vector2D(x1, y1).multSc(-1));
+       Vector AB = new Vector(new double[]{x2, y2}).sum(new Vector(new double[]{x1, y1}).scMult(-1));
+       Vector AC = new Vector(new double[]{x3, y3}).sum(new Vector(new double[]{x1, y1}).scMult(-1));
         for (int x = Math.min(x1, Math.min(x2, x3)); x <= Math.max(x1, Math.max(x2, x3)); x++) {
             for (int y = Math.min(y1, Math.min(y2, y3)); y < Math.max(y1, Math.max(y2, y3)); y++) {
-                Vector2D PA = new Vector2D(x1, y1).sum(new Vector2D(x, y).multSc(-1));
-                Vector3D V = new Vector3D(AB.x, AC.x, PA.x).CrossProd(new Vector3D(AB.y, AC.y, PA.y));
-                double u = (V.x/V.z);
-                double v = (V.y/V.z);
+                Vector PA = new Vector(new double[]{x1, y1}).sum(new Vector(new double[]{x, y}).scMult(-1));
+                Vector V = new Vector(new double[]{AB.get(0), AC.get(0), PA.get(0)}).CrossProd(new Vector(new double[]{AB.get(1), AC.get(1), PA.get(1)}));
+                double u = (V.get(0)/V.get(2));
+                double v = (V.get(1)/V.get(2));
                 if (u + v <= 1 && u >= 0 && v >= 0){
                     img.setRGB(x, y, new Color((int) (u*255), (int) (v*255), (int) ((1-u-v)*255)).getRGB());
                 }
